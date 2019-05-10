b3d47bb60f18baa55d7700e6f56337a51997365a
Jeff Lassen <Jeff@Lassens.com>
2019-05-10 12:11:23 -0700

    Removing code that resets the time inputs.

--- static/js/screenly-ose.js
+++ static/js/screenly-ose.js
@@ -553,7 +553,9 @@
       (this.$('input.time')).timepicker({
         minuteStep: 5,
         showInputs: true,
-        disableFocus: true,
+        disableFocus: true, 
+        //template: dropdown, 
+        explicitMode: true, 
         showMeridian: date_settings.show_meridian
       });
       (this.$('input[name="nocache"]')).prop('checked', this.model.get('nocache'));
@@ -601,7 +603,7 @@
           format: date_settings.datepicker_format
         });
         (this.$f(which + "_date_date")).datepicker('setValue', d.date());
-        this.$fv(which + "_date_time", d.time());
+        //this.$fv(which + "_date_time", d.time());
       }
       this.displayAdvanced();
       this.delegateEvents();
